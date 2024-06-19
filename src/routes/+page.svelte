d01b9aa1190c5f50e846a5a3206e4d11378dd6fa
Saad5400 <sdbtwa@gmail.com>
2024-06-19 23:46:23 +0300

    objects, lists, and interfaces

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1 class="btn btn-lg btn-primary">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    interface ToDoItem {
+        title: string;
+        date: Date;
+        isDone: boolean;
+    }
+
+    let toDoList: ToDoItem[] = [];
+
+    function clicked() {}
+</script>
+
+<button on:click={clicked} class="btn btn-lg btn-primary"
+    >Welcome to SvelteKit</button
+>
